4ce4dfbcff2e1ad6007e620dbe2129a6af387ca3
Your Name <jenathkanagesan@gmail.com>
2018-04-05 16:37:17 +0100

    modified sql query relates #6

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -3,17 +3,18 @@ BEGIN;
 DROP TABLE IF EXISTS spaces, reviews CASCADE;
 
 CREATE TABLE spaces (
-    id PRIMARY KEY NOT NULL,
+    id VARCHAR(100) NOT NULL,
     name VARCHAR(255) NOT NULL,
     latitude DECIMAL(10,8) NOT NULL,
-    longitude DECIMAL(10,8) NOT NULL
+    longitude DECIMAL(10,8) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE reviews (
-    id SERIAL PRIMARY KEY NOT NULL,
-    rating INT NOT NULL,
-    spacesid int FOREIGN KEY REFERENCES spaces(id)
-);
+             id SERIAL PRIMARY KEY NOT NULL,
+             rating INT NOT NULL,
+             spacesid VARCHAR(100) REFERENCES spaces(id)
+         );
 
 -- INSERT INTO spaces (id, name, latitude, longitude) VALUES ();
 
