c267aa628df7f8e4079331f3b43d3bf5ab7679cd
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-04-05 15:00:30 +0100

    add all router paths relates #3 #4

--- src/handler.js
+++ src/handler.js
@@ -1,9 +1,25 @@
 const fs = require('fs');
 const { log } = console;
+
 const staticHandler = (req, res) => {
+  log(req);
+}
+
+const spaceHandler = (req, res) => {
+  log(req);
+}
+
+const locationHandler = (req, res) => {
+  log(req);
+}
+
+const addReviewHandler = (req, res) => {
   log(req);
 }
 
 module.exports = {
   staticHandler,
+  spaceHandler,
+  locationHandler,
+  addReviewHandler,
 };
--- src/router.js
+++ src/router.js
@@ -1,14 +1,19 @@
-const { staticHandler } = require('./handler');
+const { staticHandler, spaceHandler, locationHandler, addReviewHandler } = require('./handler');
 const { log } = console;
 
 const router = (request, response) => { 
+  const { url } = request;
   if (request.method === 'GET') {
-    staticHandler(request, response);
-
+    if (url.includes('search-space')) {
+      spaceHandler(request, response);
+    } else if (url.includes('search-location')) {
+      locationHandler(request, response);
+    } else {
+      staticHandler(request, response);
+    }
   } else if (request.method === 'POST') {
-
+    addReviewHandler(request, response);
   }
-
 }
 
 module.exports = router;
