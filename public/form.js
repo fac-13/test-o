6dd4a60edaf12a0a201dfee6caff88d80781c159
Phat Ma <phat.ma@hotmail.co.uk>
2018-04-06 14:23:09 +0100

    add try catch for paring response Relates #29

--- public/form.js
+++ public/form.js
@@ -1,10 +1,20 @@
+function safelyParseJSON (json) {
+    var parsed
+    try {
+      parsed = JSON.parse(json)
+    } catch (e) {
+      console.log(e);
+    }
+    return parsed
+  }
+
 function fetch(method, url, callback) {
     var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             console.log('fetch is working', url);
-            var response = JSON.parse(xhr.responseText);
+            var response = safelyParseJSON(xhr.responseText);
             callback(response);
         } else {
             console.log('XHR error', xhr.readyState);
