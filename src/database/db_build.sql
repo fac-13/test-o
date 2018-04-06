e11372b97e1a2f1c8f16a7c8886b92781cbc93c9
Nicos <nick.zonias@gmail.com>
2018-04-06 10:50:16 +0100

    build script and test env Relates to #22

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,7 +1,7 @@
 BEGIN;
     DROP TABLE IF EXISTS spaces, reviews
     CASCADE;
-/* To create a database locally or on the server*/
+-- To create a database locally or on the server--
 CREATE TABLE spaces
 (
     id VARCHAR(100) NOT NULL,
@@ -16,11 +16,11 @@ CREATE TABLE reviews
     rating INT NOT NULL,
     spacesid VARCHAR(100) REFERENCES spaces(id)
 );
-/* Please uncomment the following if you are using a local database and would like to populate it with dummy data */
+-- Please uncomment the following if you are using a local database and would like to populate it with dummy data --
 INSERT INTO spaces
     (id, name, latitude, longitude)
 VALUES
-    ('ChIJnX2njGIPdkgRJSUCk-BqQsk', "Chit Chaat Chai", 51.4600145, -0.1890872),
-    ('ChIJrXOlLNYadkgRiohqRNt8BSY', "Oriental Dragon", 51.5154956, -0.1493805),
-    ('ChIJ-TGDfdQdkgRIZht1FeEQeg', "WorkSpace Design", 51.5175847, -0.146326)
+    ('ChIJnX2njGIPdkgRJSUCk-BqQsk', 'Chit Chaat Chai', 51.4600145, -0.1890872),
+    ('ChIJrXOlLNYadkgRiohqRNt8BSY', 'Oriental Dragon', 51.5154956, -0.1493805),
+    ('ChIJ-TGDfdQdkgRIZht1FeEQeg', 'WorkSpace Design', 51.5175847, -0.146326);
 COMMIT;
--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,8 +1,23 @@
 // Add code below to query your database
 const dbConnection = require("../database/db_connection.js");
 
-const getData = (cb) => {
-  dbConnection.query(`SELECT * FROM spaces LEFT JOIN reviews ON spaces.id = reviews.spacesid`, (err, res) => {
+const getData = cb => {
+  dbConnection.query(
+    `SELECT * FROM spaces LEFT JOIN reviews ON spaces.id = reviews.spacesid`,
+    (err, res) => {
+      if (err) {
+        cb(err);
+        console.log(err);
+      } else {
+        cb(null, res.rows);
+        console.log(res.rows);
+      }
+    }
+  );
+};
+
+const getSpaces = cb => {
+  dbConnection.query(`SELECT * FROM spaces`, (err, res) => {
     if (err) {
       cb(err);
     } else {
@@ -10,5 +25,9 @@ const getData = (cb) => {
     }
   });
 };
+getSpaces((err, res) => {
+  if (err) return console.log(err);
+  console.log(res);
+});
 
-module.exports = getData;
+module.exports = { getData, getSpaces };
